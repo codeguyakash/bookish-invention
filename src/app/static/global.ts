2cf83bb7af31fa175074934a91ab23a0f4e14a50
codeguyakashme <github@codeguyakash.me>
2024-09-01 05:23:36 -0700

    add missing file

--- src/app/static/global.ts
+++ src/app/static/global.ts
@@ -2,3 +2,17 @@ export const clientid = 0;
 export const campId = 110;
 export const leadId = 587;
 export const crmCallid = 0;
+
+
+export const APIS = {
+  GET_SITE_SURVEYOR_LIST: "",
+  GET_SUBMITTED_HANDOFF_LIST: ""
+}
+// Roles
+export const SITE_SURVEYOR: number = 1;
+export const CONSUMER_ROLE: number = 2;
+
+export const ROLES = {
+  SITE_SURVEYOR: SITE_SURVEYOR,
+  CONSUMER_ROLE: CONSUMER_ROLE,
+};
---
+++ src/app/static/static-config.ts
@@ -0,0 +1,6 @@
+export const vars = {
+  SEARCH_RESULT_DOWNLOAD_PATH: '/api/search-result/download',
+  API_BASE_URL: "https://api.solarbyluminous.com",
+  STRIP_IMAGE_PATH: '/api/search-result/download'
+
+};
